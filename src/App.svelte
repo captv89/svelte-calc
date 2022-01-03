cc78360de92db8dc58c0c3319ccf73aedfb975d9
captv89 <vishnu.raveendranathan@gmail.com>
2022-01-04 04:11:59 +0530

    reworked the code based on steven bell example

--- src/App.svelte
+++ src/App.svelte
@@ -1,123 +1,42 @@
 <script>
-  import MainFrame from "./components/MainFrame.svelte";
-  import TextArea from "./components/TextArea.svelte";
-  import CustomButton from "./components/CustomButton.svelte";
-  import ShowHistory from "./components/ShowHistory.svelte";
-  import { history } from "./store.js";
-
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  let operators = ["+", "-", "*", "/"];
-  let mathOps = "";
-  let result = undefined;
-  let showPastHistory = false;
-  const clearAll = () => {
-    mathOps = "";
-    result = undefined;
-    console.log("Clear All Clicked", mathOps);
-  };
-  const showResult = () => {
-    result = eval(mathOps);
-    addToHistory(mathOps, result);
-    console.log("Show Result Clicked", result, $history);
-  };
-  const keepWriting = (value) => {
-    mathOps += value;
-    console.log("Keep Writing Clicked", value);
-  };
-  const clearLast = () => {
-    mathOps = mathOps.slice(0, -1);
-    console.log("Clear Last Clicked", mathOps);
-  };
-  const toggleHistory = () => {
-    showPastHistory = !showPastHistory;
-    console.log("Toggle History Clicked", showPastHistory);
-  };
-  const addToHistory = (mathOps, result) => {
-    $history = [
-      ...$history,
-      {
-        operation: mathOps,
-        result: result,
-      },
-    ];
-    console.log("History", history);
-  };
-  const clearHistory = () => {
-    history.length = 0;
-    console.log("History", history);
-  };
-  const clearLastHistory = () => {
-    history.pop();
-    console.log("History", history);
-  };
+	import Calculator from './components/Calculator.svelte';
 </script>
 
-<main>
-  <MainFrame>
-    {#if result !== undefined}
-      <TextArea operation={mathOps} {result} />
-    {:else}
-      <TextArea operation={mathOps} result="" />
-    {/if}
-    <!-- Div for Numbers -->
-    <div class="numbers">
-      {#each numbers as number}
-        <CustomButton
-          on:click={() => keepWriting(number)}
-          props={number}
-          myClass="numbers"
-        />
-      {/each}
-    </div>
-    <!-- Div for Operators -->
-    <div class="operators">
-      {#each operators as operator}
-        <CustomButton
-          on:click={() => keepWriting(operator)}
-          props={operator}
-          myClass="operators"
-        />
-      {/each}
+<svelte:head>
+	<!-- Fonts --><link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+	/>
 
-      <!-- Clear operation -->
-      <CustomButton on:click={clearAll} props="AC" myClass="clear" />
-      <!-- Backspace -->
-      <CustomButton on:click={clearLast} props="C" myClass="clear" />
-      <!-- Equals -->
-      <CustomButton on:click={showResult} props="=" myClass="equal" />
-      <!-- Show History -->
-      <CustomButton
-        on:click={toggleHistory}
-        props="History"
-        myClass="history"
-      />
-    </div>
-  </MainFrame>
-  {#if showPastHistory}
-    <ShowHistory items={$history} />
-  {/if}
-</main>
+	<!-- Pixel Font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
+		rel="stylesheet"
+	/>
 
-<style>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
-    margin: 0 0;
-    background-color: bisque;
-  }
-  .numbers {
-    justify-content: space-evenly;
-    align-items: center;
-    float: left;
-    width: 65%;
-  }
-  .operators {
-    justify-content: space-evenly;
-    align-items: center;
-    float: right;
-    width: 35%;
-  }
-</style>
+	<!-- Material Typography --><link
+		rel="stylesheet"
+		href="https://unpkg.com/@material/typography@13.0.0/dist/mdc.typography.css"
+	/>
+
+	<!-- SMUI --><link
+		rel="stylesheet"
+		href="https://unpkg.com/svelte-material-ui/bare.css"
+	/>
+
+	<style>
+		body {
+			background: #272828;
+			height: 100vh;
+			overflow: hidden;
+		}
+	</style>
+</svelte:head>
+
+<Calculator />
---
+++ src/components/Display.svelte
@@ -0,0 +1,125 @@
+<script>
+	export let total;
+	export let totalArr;
+	export let buttons;
+</script>
+
+<div class="calculator">
+	<div id="display">
+		{#if total !== 'err'}
+			<div>
+				<div class="top">{totalArr.join(' ')}</div>
+			</div>
+			<div>
+				<div class="bottom">{total}</div>
+			</div>
+		{:else}
+			<div>{total}</div>
+		{/if}
+	</div>
+	{#each buttons as button}
+		<button id={button.id} on:click={button.func}>{button.value}</button>
+	{/each}
+</div>
+
+<style>
+	.calculator {
+		display: grid;
+		grid-template-columns: 24% 24% 24% 24%;
+		grid-gap: 3px;
+		background-color: rgb(226, 226, 226);
+		box-shadow: 0px 0px 16px 2px #ccc;
+		border-radius: 8px;
+		margin: 5vh auto;
+		width: 70vw;
+		max-width: 250px;
+		min-width: 200px;
+		padding: 5px;
+		user-select: none;
+		justify-content: space-around;
+		align-items: stretch;
+		align-content: space-evenly;
+	}
+
+	#display {
+		grid-column: 1 / span 4;
+		height: 120px;
+		border: solid 2px black;
+		border-radius: 2px;
+		cursor: initial;
+		padding: 0 5px;
+		margin-bottom: 1rem;
+		font-family: 'VT323';
+		background-color: #f4f4f4;
+	}
+
+	#display > div {
+		text-align: right;
+		box-sizing: border-box;
+		height: 70%;
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+		text-align: right;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+
+	#display > div > div {
+		float: right;
+	}
+
+	.top {
+		font-size: 1.3em;
+		line-height: 200%;
+	}
+
+	.bottom {
+		font-size: 2em;
+		line-height: 100%;
+	}
+
+	button {
+		background-color: #f4f4f4;
+		color: black;
+		border-radius: 5px;
+		text-align: center;
+		text-decoration: none;
+		outline: none;
+        font-family: 'VT323';
+		font-weight: 400;
+		font-size: 1.2em;
+		transition-duration: 0.4s;
+		cursor: pointer;
+		box-shadow: 0 4px #999;
+	}
+
+	button:active {
+		background-color: #cecece;
+		outline: none;
+		box-shadow: 0 1px #666;
+		transform: translateY(4px);
+	}
+
+	#ac {
+		grid-column: 1 / span 2;
+	}
+
+	#clear {
+		grid-column: 3 / span 2;
+	}
+
+	#add {
+		height: initial;
+		grid-column: 4;
+		grid-row: 4 / span 2;
+	}
+	#equal {
+		height: initial;
+		grid-column: 4;
+		grid-row: 6 / span 2;
+	}
+	#zero {
+		grid-column: 1 / span 2;
+	}
+</style>
